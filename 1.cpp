1a1e83623dd4aa0f579a05262492d1c33d05ec5e
Odihochka <zhekasam443322@gmail.com>
2026-01-21 07:48:14 +0000

    l1

---
+++ 1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Самолюк Євгеній" << std::endl;
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+    std::cout << "ВІЗИТКА СТУДЕНТА\n";
+    std::cout << "Прізвище:\tСамолюк\n";
+    std::cout << "Група:\t\tП-23\n";
+    std::cout << "Навчальний заклад:\tКоледж\n";
+    return 0;
+}
